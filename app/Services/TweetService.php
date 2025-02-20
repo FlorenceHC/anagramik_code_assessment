dadf0b9ce5d8107ef96ab6fce0350e1484c98a88
Aleksandar Radivojević <anagramik@gmail.com>
2025-02-20 12:10:34 +0100

    [ADD] pagination to TweetService

--- app/Services/TweetService.php
+++ app/Services/TweetService.php
@@ -12,21 +12,41 @@ class TweetService
     private const API_TOKEN = '8c5996d5-fb89-46c9-8821-7063cfbc18b1';
     private const CACHE_TTL = 1800; // 30 minutes in seconds
 
-    public function getTweets(string $userName): array
+    public function getTweetsFromApi(string $userName): array
+    {
+        $response = Http::withToken(self::API_TOKEN)
+            ->get(self::API_URL, [
+                'userName' => $userName
+            ]);
+
+        if ($response->failed()) {
+            throw new \Exception('Failed to fetch tweets: ' . $response->status());
+        }
+
+        return $response->json();
+    }
+
+    public function getTweets(string $userName, int $page = 1, int $perPage = 10): array
     {
         $cacheKey = "tweets.{$userName}";
 
-        return Cache::remember($cacheKey, self::CACHE_TTL, function () use ($userName) {
-            $response = Http::withToken(self::API_TOKEN)
-                ->get(self::API_URL, [
-                    'userName' => $userName
-                ]);
-
-            if ($response->failed()) {
-                throw new \Exception('Failed to fetch tweets: ' . $response->status());
-            }
-
-            return $response->json();
+        $tweets = Cache::remember($cacheKey, self::CACHE_TTL, function () use ($userName) {
+            return $this->getTweetsFromApi($userName);
         });
+
+        $offset = ($page - 1) * $perPage;
+        $totalTweets = count($tweets);
+        $totalPages = ceil($totalTweets / $perPage);
+
+        return [
+            'tweets' => array_slice($tweets, $offset, $perPage),
+            'pagination' => [
+                'current_page' => $page,
+                'per_page' => $perPage,
+                'total_items' => $totalTweets,
+                'total_pages' => $totalPages,
+                'has_more' => $page < $totalPages
+            ],
+        ];
     }
 }
--- tests/Unit/Services/TweetServiceTest.php
+++ tests/Unit/Services/TweetServiceTest.php
@@ -40,7 +40,16 @@ class TweetServiceTest extends TestCase
                 $request['userName'] === $userName;
         });
 
-        $this->assertEquals($mockTweets, $result);
+        $this->assertEquals([
+            'tweets' => $mockTweets,
+            'pagination' => [
+                'current_page' => 1,
+                'per_page' => 10,
+                'total_items' => 1,
+                'total_pages' => 1,
+                'has_more' => false
+            ]
+        ], $result);
     }
 
     public function test_in_case_of_consecutive_calls_cached_data_should_be_returned_and_only_one_http_request_should_be_triggered()
@@ -93,5 +102,68 @@ class TweetServiceTest extends TestCase
         }
 
         $this->fail('Exception has not been thrown');
+    }
+
+    public function test_it_should_return_paginated_data_if_there_are_too_many_tweets()
+    {
+        // Arrange
+        $userName = 'joe_smith';
+        $mockTweets = [
+            [
+                'id' => '52f83d7c-ad2c-4ca0-b742-b03bc27f0c96',
+                'createdAt' => '2017-12-01T11:12:42',
+                'text' => 'Test tweet',
+                'user' => [
+                    'id' => '75343078-b5dd-306f-a3f9-8203a3915144',
+                    'userName' => 'joe_smith'
+                ]
+            ],
+            [
+                'id' => '52f83d7c-ad2c-4ca0-b742-b03bc27f0c96',
+                'createdAt' => '2017-12-01T11:12:42',
+                'text' => 'Test tweet 2',
+                'user' => [
+                    'id' => '75343078-b5dd-306f-a3f9-8203a3915144',
+                    'userName' => 'joe_smith2'
+                ]
+            ],
+            [
+                'id' => '52f83d7c-ad2c-4ca0-b742-b03bc27f0c96',
+                'createdAt' => '2017-12-01T11:12:42',
+                'text' => 'Test tweet 3',
+                'user' => [
+                    'id' => '75343078-b5dd-306f-a3f9-8203a3915144',
+                    'userName' => 'joe_smith'
+                ]
+            ]
+        ];
+
+        // Act
+        Http::fake([
+            'app.codescreen.com/api/assessments/tweets*' => Http::response($mockTweets, 200)
+        ]);
+
+        $tweetService = new TweetService();
+        $result = $tweetService->getTweets($userName, 2, 1);
+
+        // Assert
+        Http::assertSent(function (Request $request) use ($userName, $tweetService) {
+            return $request->url() === "https://app.codescreen.com/api/assessments/tweets?userName=joe_smith" &&
+                $request->header('Authorization')[0] === 'Bearer 8c5996d5-fb89-46c9-8821-7063cfbc18b1' &&
+                $request['userName'] === $userName;
+        });
+
+        $this->assertEquals([
+            'tweets' => [
+                $mockTweets[1] // only one tweet
+            ],
+            'pagination' => [
+                'current_page' => 2, // we are on the second page
+                'per_page' => 1, // we want only 1 tweet
+                'total_items' => 3, // we have 3 tweets
+                'total_pages' => 3, // we have 3 tweets
+                'has_more' => true
+            ]
+        ], $result);
     }
 }
